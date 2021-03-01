7aa86b3e55256cbe357ba78b54897c304a19685b
Pukki Konstantin <pukkikonstantin@gmail.com>
2021-03-01 17:34:19 +0300

    Настройка webpack + eslint

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+/*eslint-disable*/
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+  },
+  // resolve: {
+  //   alias: {
+  //     '@styles': path.resolve(__dirname, 'src/styles'),
+  //     '@img': path.resolve(__dirname, 'src/public/img'),
+  //     '@modules': path.resolve(__dirname, 'src/app/modules'),
+  //   }
+  // },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './public/index.html'
+    }),
+    // new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|svg|webp)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      }
+    ]
+  }
+}
